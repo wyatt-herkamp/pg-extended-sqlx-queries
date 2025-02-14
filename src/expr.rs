36f5d6292937571510927f6c659c5f7253e08315
Wyatt Herkamp <wherkamp@gmail.com>
2025-02-14 06:22:56 -0500

    Added more comparisons for ExprType

--- src/expr.rs
+++ src/expr.rs
@@ -30,22 +30,14 @@ pub trait ExprType<'args> {
         Self: Sized + 'args,
         E: ExprType<'args> + 'args,
     {
-        FilterConditionBuilder::CompareValue {
-            left: Box::new(self),
-            comparison: SQLComparison::Equals,
-            right: Box::new(value),
-        }
+        self.compare(SQLComparison::Equals, value)
     }
     fn like<E>(self, value: E) -> FilterConditionBuilder<'args>
     where
         Self: Sized + 'args,
         E: ExprType<'args> + 'args,
     {
-        FilterConditionBuilder::CompareValue {
-            left: Box::new(self),
-            comparison: SQLComparison::Like,
-            right: Box::new(value),
-        }
+        self.compare(SQLComparison::Like, value)
     }
     fn is_not_null(self) -> FilterConditionBuilder<'args>
     where
@@ -58,6 +50,57 @@ pub trait ExprType<'args> {
         Self: Sized + 'args,
     {
         FilterConditionBuilder::Null(Box::new(self))
+    }
+    fn compare<L>(self, comparison: SQLComparison, value: L) -> FilterConditionBuilder<'args>
+    where
+        Self: Sized + 'args,
+        L: ExprType<'args> + 'args,
+    {
+        FilterConditionBuilder::CompareValue {
+            left: Box::new(self),
+            comparison,
+            right: Box::new(value),
+        }
+    }
+    fn less_than<E>(self, value: E) -> FilterConditionBuilder<'args>
+    where
+        Self: Sized + 'args,
+        E: ExprType<'args> + 'args,
+    {
+        self.compare(SQLComparison::LessThan, value)
+    }
+    fn less_than_or_equals<E>(self, value: E) -> FilterConditionBuilder<'args>
+    where
+        Self: Sized + 'args,
+        E: ExprType<'args> + 'args,
+    {
+        self.compare(SQLComparison::LessThanOrEqualTo, value)
+    }
+
+    fn greater_than<E>(self, value: E) -> FilterConditionBuilder<'args>
+    where
+        Self: Sized + 'args,
+        E: ExprType<'args> + 'args,
+    {
+        self.compare(SQLComparison::GreaterThan, value)
+    }
+    fn greater_than_or_equals<E>(self, value: E) -> FilterConditionBuilder<'args>
+    where
+        Self: Sized + 'args,
+        E: ExprType<'args> + 'args,
+    {
+        self.compare(SQLComparison::GreaterThanOrEqualTo, value)
+    }
+    fn between<L, R>(self, start: L, end: R) -> FilterConditionBuilder<'args>
+    where
+        Self: Sized + 'args,
+        L: ExprType<'args> + 'args,
+        R: ExprType<'args> + 'args,
+    {
+        FilterConditionBuilder::Between {
+            start: Box::new(start),
+            end: Box::new(end),
+        }
     }
 }
 
