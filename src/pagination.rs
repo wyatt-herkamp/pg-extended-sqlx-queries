c30d0e83ba9cd2ed763a6ce2dbf70688768e9844
Wyatt Herkamp <wherkamp@gmail.com>
2025-04-16 17:15:01 -0400

    Kill Old Page Params

--- src/pagination.rs
+++ src/pagination.rs
@@ -11,8 +11,7 @@ pub trait PaginationSupportingTool {
     /// Set the offset for the query
     fn offset(&mut self, offset: i32) -> &mut Self;
     /// Set the page parameters for the query
-    fn page_params(&mut self, page_params: impl Into<PageParams>) -> &mut Self {
-        let page_params = page_params.into();
+    fn page_params(&mut self, page_params: impl PageParamsType) -> &mut Self {
         self.limit(page_params.limit()).offset(page_params.offset())
     }
 }
@@ -23,12 +22,40 @@ pub trait PaginationOwnedSupportingTool {
     /// Set the offset for the query
     fn offset(self, offset: i32) -> Self;
     /// Set the page parameters for the query
-    fn page_params(self, page_params: impl Into<PageParams>) -> Self
+    fn page_params(self, page_params: impl PageParamsType) -> Self
     where
         Self: Sized,
     {
-        let page_params = page_params.into();
         self.limit(page_params.limit()).offset(page_params.offset())
+    }
+}
+pub trait PageParamsType {
+    /// The page size
+    fn page_size(&self) -> i32;
+    /// The page number
+    fn page_number(&self) -> i32;
+
+    /// Calculate the number of pages based on the total number of items
+    #[inline]
+    fn number_of_pages(&self, total: i64) -> i32 {
+        (total as f64 / self.page_size() as f64).ceil() as i32
+    }
+    #[inline]
+    fn limit(&self) -> i32 {
+        self.page_size()
+    }
+    /// Requests start at 1.
+    /// However, offset starts at 0.
+    ///
+    /// This function returns the index of the page.
+    #[inline]
+    fn page_index(&self) -> i32 {
+        (self.page_number() - 1).max(0)
+    }
+    /// Requests start at 1.
+    #[inline]
+    fn offset(&self) -> i32 {
+        self.page_size() * self.page_index()
     }
 }
 /// Parameters for pagination
@@ -39,6 +66,7 @@ pub trait PaginationOwnedSupportingTool {
 /// Passing a page number less than 1 or equal to I32::MAX might result in all items being returned
 /// This is dependent on the request handler
 #[derive(Clone, Copy, PartialEq, Eq)]
+#[deprecated]
 
 pub struct PageParams {
     /// The number of items per page
